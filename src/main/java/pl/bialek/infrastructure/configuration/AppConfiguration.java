d6262f7640180743cea1b91360e5522590629eba
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-27 12:17:14 +0200

    conf: Merging JPA config with AppConfiguration. Providing HikariConfiguration and using Environment variables.

--- src/main/java/pl/bialek/infrastructure/configuration/AppConfiguration.java
+++ src/main/java/pl/bialek/infrastructure/configuration/AppConfiguration.java
@@ -1,12 +1,109 @@
 package pl.bialek.infrastructure.configuration;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.Location;
+import org.flywaydb.core.api.configuration.ClassicConfiguration;
+import org.hibernate.cfg.Environment;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.*;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.bialek.ComponentScanMarker;
+import pl.bialek.infrastructure.database.entity._EntityMarker;
+import pl.bialek.infrastructure.database.repository.jpa._JpaMarker;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
+@AllArgsConstructor
+@EnableJpaRepositories(basePackageClasses = _JpaMarker.class)
+@PropertySource({"classpath:database.properties"})
+@EnableTransactionManagement
+@EnableWebMvc
 @ComponentScan(basePackageClasses = ComponentScanMarker.class)
-@Import(JPAConfiguration.class )
-public class AppConfiguration {
+public class AppConfiguration implements WebMvcConfigurer, ApplicationContextAware {
+
+
+    private final org.springframework.core.env.Environment environment;
+
+    @Setter
+    private ApplicationContex applicationContext;
+
+    @Bean
+    @DependsOn("flyway")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan(_EntityMarker.class.getPackageName());
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setJpaProperties(jpaProperties());
+        return entityManagerFactoryBean;
+    }
+
+    private Properties jpaProperties() {
+        final Properties properties = new Properties();
+
+        properties.setProperty(Environment.DIALECT, environment.getProperty(Environment.DIALECT));
+        properties.setProperty(Environment.HBM2DDL_AUTO, environment.getProperty(Environment.HBM2DDL_AUTO));
+        properties.setProperty(Environment.SHOW_SQL, environment.getProperty(Environment.SHOW_SQL));
+        properties.setProperty(Environment.FORMAT_SQL, environment.getProperty(Environment.FORMAT_SQL));
+        return properties;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(final EntityManagerFactory entityManagerFactory) {
+        final JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslator() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    @Bean(initMethod = "migrate")
+    Flyway flyway() {
+        ClassicConfiguration configuration = new ClassicConfiguration();
+        configuration.setBaselineOnMigrate(true);
+        configuration.setLocations(new Location("filesystem:src/main/resources/flyway/migrations"));
+        configuration.setDataSource(dataSource());
+        return new Flyway(configuration);
+    }
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty(Environment.DRIVER)));
+        dataSource.setUrl(environment.getProperty(Environment.URL));
+        dataSource.setUsername(environment.getProperty(Environment.USER));
+        dataSource.setPassword(environment.getProperty(Environment.PASS));
+
+        hikariConfig.setConnectionTestQuery("SELECT 1");
+        hikariConfig.setPoolName("springHikariCP");
+
+        hikariConfig.setMaximumPoolSize(20);
+        hikariConfig.setConnectionTimeout(20000);
+        hikariConfig.setMinimumIdle(10);
+        hikariConfig.setIdleTimeout(300000);
+
+        return new HikariDataSource(hikariConfig);
+    }
+
 }
