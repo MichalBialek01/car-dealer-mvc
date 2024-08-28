762e6c126cc40a8a1a330439745656160295979a
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-28 17:20:45 +0200

    feat: Providing correct DTO for Controller - Domain layer

--- src/main/java/pl/bialek/api/dto/MechanicDTO.java
+++ src/main/java/pl/bialek/api/dto/MechanicDTO.java
@@ -1,4 +1,16 @@
 package pl.bialek.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MechanicDTO {
+    String name;
+    String surname;
+    String pesel;
 }
