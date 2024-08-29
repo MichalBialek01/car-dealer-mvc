f9930a03707235de83f2bb14ec3c692e6ade52a6
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-29 13:27:24 +0200

    feat: Providing car_purchase_processed controller and page

--- src/main/java/pl/bialek/api/controller/PurchaseController.java
+++ src/main/java/pl/bialek/api/controller/PurchaseController.java
@@ -2,13 +2,19 @@ package pl.bialek.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.bialek.api.dto.CarPurchaseRequestDTO;
 import pl.bialek.api.dto.CarToBuyDTO;
 import pl.bialek.api.dto.mapper.CarDtoMapper;
 import pl.bialek.api.dto.mapper.CarPurchaseRequestDtoMapper;
 import pl.bialek.business.CarPurchaseService;
+import pl.bialek.domain.CarPurchaseRequest;
+import pl.bialek.domain.Invoice;
 import pl.bialek.domain.Salesman;
 
 import java.util.List;
@@ -51,4 +57,30 @@ public class PurchaseController {
         );
 
     }
+    @PostMapping(value = PURCHASE)
+    public String makePurchase(
+            @ModelAttribute("CarPurchaseRequestDTO") CarPurchaseRequestDTO carPurchaseRequestDTO,
+            BindingResult bindingResult,
+            ModelMap model
+    ) {
+        if(bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(error -> System.out.println(error));
+            return "error";
+        }
+        CarPurchaseRequest carPurchaseRequest = carPurchaseRequestDtoMapper.mapFromDTO(carPurchaseRequestDTO);
+        Invoice invoice = carPurchaseService.purchase(carPurchaseRequest);
+
+        if(!carPurchaseRequestDTO.getExistingCustomerEmail().isBlank()){
+            model.addAttribute("existingCustomerEmail",carPurchaseRequestDTO.getExistingCustomerEmail());
+        }else{
+            model.addAttribute("customerName", carPurchaseRequestDTO.getCustomerName());
+            model.addAttribute("customerSurname", carPurchaseRequestDTO.getCustomerSurname());
+        }
+        model.addAttribute("invoiceNumber", invoice.getInvoiceNumber());
+
+        return "car_purchase_processed";
+    }
+
+
+
 }
--- src/main/java/pl/bialek/business/CarPurchaseService.java
+++ src/main/java/pl/bialek/business/CarPurchaseService.java
@@ -6,45 +6,63 @@ import pl.bialek.domain.*;
 
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class CarPurchaseService {
 
-//That class provides functionality to buy a car by costumers, it means that seller issues invoice for certain car.
-
-//    private final DataPreparationService dataPreparationService;
     private final CustomerService customerService;
     private final CarService carService;
     private final SalesmanService salesmanSercice;
 
-//    public void purchase() {
-//        var firstTimeData = dataPreparationService.prepareFirstTimePurchaseData();
-//        var nextTimeData = dataPreparationService.prepareNextTimePurchaseData();
-//
-//        List<Customer> firstTimeCustomers = firstTimeData.stream()
-//                .map(this::createFirtTimeToBuyCustomer)
-//                .toList();
-//        firstTimeCustomers.forEach(customerService::issueInvoice);
-//
-//        List<Customer> nextTimeCustomers = nextTimeData.stream()
-//                .map(this::createNextTimeToBuyCustomer)
-//                .toList();
-//        nextTimeCustomers.forEach(customerService::issueInvoice);
-//    }
+    public Invoice purchase(CarPurchaseRequest carPurchaseRequest) {
 
-//    private Customer createFirtTimeToBuyCustomer(CarPurchaseRequestInputData inputData) {
-//        //Catching car - having single value (VIN)
-//        CarToBuy car = carService.findCarToBuy(inputData.getCarVin());
-//        //Caching Salesman - having single value (PESEL)
-//        Salesman salesman = salesmanSercice.findSalesman(inputData.getSalesmanPesel());
-//        //Building Invoice basing on buingCar and salesman
-//        Invoice invoice = buildInvoice(car, salesman);
-//
-//        return dataPreparationService.createCustomer(inputData, invoice);
-//
-//
-//    }
+        return carPurchaseRequest.getExistingCustomerEmail().isBlank()
+                ? processFirstTimeCustomer(carPurchaseRequest)
+                : processNextTimeCustomer(carPurchaseRequest);
+    }
+
+    private Invoice processFirstTimeCustomer(CarPurchaseRequest carPurchaseRequest) {
+        CarToBuy carToBuy = carService.findCarToBuy(carPurchaseRequest.getCarVin());
+        Salesman salesman = salesmanSercice.findSalesman(carPurchaseRequest.getSalesmanPesel());
+        Invoice invoice = buildInvoice(carToBuy, salesman);
+
+        Customer customer = buildCustomer(carPurchaseRequest,invoice);
+        customerService.issueInvoice(customer);
+        return invoice;
+    }
+
+    private Invoice processNextTimeCustomer(CarPurchaseRequest carPurchaseRequest) {
+            Customer existingCustomer = customerService.findCustomer(carPurchaseRequest.getExistingCustomerEmail());
+            CarToBuy car = carService.findCarToBuy(carPurchaseRequest.getCarVin());
+            Salesman salesman = salesmanSercice.findSalesman(carPurchaseRequest.getSalesmanPesel());
+            Invoice invoice = buildInvoice(car, salesman);
+            Set<Invoice> existingInvoices = existingCustomer.getInvoices();
+            existingInvoices.add(invoice);
+            customerService.issueInvoice(existingCustomer.withInvoices(existingInvoices));
+            return invoice;
+    }
+
+
+    private Customer buildCustomer(CarPurchaseRequest carPurchaseRequest, Invoice invoice) {
+        return Customer.builder()
+                .name(carPurchaseRequest.getCustomerName())
+                .surname(carPurchaseRequest.getCustomerSurname())
+                .phone(carPurchaseRequest.getCustomerPhone())
+                .email(carPurchaseRequest.getCustomerEmail())
+                .address(
+                        Address.builder()
+                                .country(carPurchaseRequest.getCustomerAddressCountry())
+                                .city(carPurchaseRequest.getCustomerAddressCity())
+                                .postalCode(carPurchaseRequest.getCustomerAddressPostalCode())
+                                .address(carPurchaseRequest.getCustomerAddressStreet())
+                                .build()
+                )
+                .invoices(Set.of(invoice))
+                .build();
+    }
 
     private Customer createNextTimeToBuyCustomer(CarPurchaseRequestInputData inputData) {
         Customer exisitngCustomer = customerService.findCustomer(inputData.getCustomerEmail());
